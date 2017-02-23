b2161f1a9490e0c91822975f7f25307877b2df3c
Novák Tamás <novaktamas27@gmail.com>
2017-02-23 17:24:45 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.scr = New System.Windows.Forms.HScrollBar()
+        Me.SuspendLayout()
+        '
+        'scr
+        '
+        Me.scr.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.scr.Location = New System.Drawing.Point(0, 644)
+        Me.scr.Maximum = 900
+        Me.scr.Name = "scr"
+        Me.scr.Size = New System.Drawing.Size(884, 17)
+        Me.scr.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(884, 661)
+        Me.Controls.Add(Me.scr)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents scr As HScrollBar
+End Class
---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Dim p() As Point
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+
+
+
+
+    End Sub
+
+    Private Sub HScrollBar1_Scroll(sender As Object, e As ScrollEventArgs) Handles scr.Scroll
+        Dim p() As Point = {New Point(scr.Value, 450), New Point(scr.Value + 75, 30), New Point(scr.Value + 150, 400)}
+        Me.CreateGraphics.DrawCurve(Pens.Black, p, 1)
+    End Sub
+End Class
